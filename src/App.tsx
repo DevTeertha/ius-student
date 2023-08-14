ea4ab41f3d5e45268562286e0d6c7c86cfe4aeea
Tertha Dev Sarker <devteertha28@gmail.com>
2023-08-15 02:53:33 +0600

    Fix Whole Login Flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
 import './App.css';
 
+import { useState } from 'react';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Dashboard from './pages/dashboard';
+import AddStudent from './pages/dashboard/add-student';
 
 import { authGuard, notAuthGuard } from './shared/guard/auth/authGuard';
+
+import Toast, { ToastContext } from './shared/components/toast/Toast';
+
+import { EToastStatusType } from './shared/interface/toast.interface';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
+  },
+  {
+    path: '/dashboard',
+    element: <Dashboard />,
+    loader: authGuard,
+  },
+  {
+    path: '/dashboard/add-student',
+    element: <AddStudent />,
     loader: authGuard,
   },
   {
@@ -21,7 +37,22 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  const [isError, setIsError] = useState<boolean>(false);
+  const [isSuccess, setIsSuccess] = useState<boolean>(false);
+  const [message, setMessage] = useState<string>('');
+
+  return (
+    <ToastContext.Provider
+      value={{
+        errorState: [isError, setIsError],
+        successState: [isSuccess, setIsSuccess],
+        messageState: [message, setMessage],
+      }}>
+      {isSuccess && <Toast status={EToastStatusType.SUCCESS} state={[isSuccess, setIsSuccess]} message={message} />}
+      {isError && <Toast status={EToastStatusType.ERROR} state={[isError, setIsError]} message={message} />}
+      <RouterProvider router={router} />
+    </ToastContext.Provider>
+  );
 }
 
 export default App;
--- src/components/dashboard/DashboardComponent.tsx
+++ src/components/dashboard/DashboardComponent.tsx
@@ -1,25 +1,22 @@
-import NavbarComponent from '../navbar/navbar';
+import { Link } from 'react-router-dom';
+import HomeComponent from '../home/HomeComponent';
+import NavbarComponent from '../navbar/NavbarComponent';
 
-function HomeComponent() {
+function DashboardComponent() {
   return (
     <>
       <NavbarComponent />
-      <div className='container'>
-        <div className='text-center'>
-          <div className='form-control'>
-            <div className='input-group input-group-sm'>
-              <input type='text' placeholder='Search…' className='input input-bordered' />
-              <button className='btn btn-square'>
-                <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
-                  <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z' />
-                </svg>
-              </button>
-            </div>
+      <div className='container px-5 py-5'>
+        <HomeComponent showAddButton={true}>
+          <div className='text-end'>
+            <Link to={'/dashboard/add-student'}>
+              <button className='btn btn-dark bg-gray-800 text-white hover:bg-gray-900 hover:text-white w-4/5'>Add Student</button>
+            </Link>
           </div>
-        </div>
+        </HomeComponent>
       </div>
     </>
   );
 }
 
-export default HomeComponent;
+export default DashboardComponent;
