dd4ec55b4dab3d79deaf0d30c9b24fa3144ece50
DevTeertha <devteertha28@gmail.com>
2023-09-07 00:03:25 +0600

    Config Database

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { EducationModule } from './education/education.module';
 import { ExperienceModule } from './experience/experience.module';
@@ -10,22 +11,26 @@ import { MediaModule } from './media/media.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'ius_student',
-      entities: [],
-      autoLoadEntities: true,
-      keepConnectionAlive: false,
-      synchronize: true,
-      extra: {
-        decimalNumbers: true,
-      },
-      bigNumberStrings: false,
-      logging: false,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get('MYSQL_HOST'),
+        port: config.get('MYSQL_PORT'),
+        username: config.get('MYSQL_USERNAME'),
+        password: config.get('MYSQL_PASSWORD'),
+        database: config.get('MYSQL_DB_NAME'),
+        entities: [],
+        autoLoadEntities: true,
+        keepConnectionAlive: false,
+        synchronize: true,
+        extra: {
+          decimalNumbers: true,
+        },
+        bigNumberStrings: false,
+        logging: false,
+      }),
     }),
     AuthModule,
     StudentModule,
